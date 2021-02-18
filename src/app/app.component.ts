0e33105bdd804866aa419e04593affbe20f93531
aleksandrew <andrew.alexandrow97@gmail.com>
2021-02-18 15:46:24 +0200

    binding part one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dynamic title';
-  number = 42 / 2 * 115;
-  arr = [1, 2, 3];
-  obj = { a: 1, b: {c: 2}};
 
+  inputValue = '';
 
-  img = 'https://www.inovex.de/blog/wp-content/uploads/2022/01/one-year-of-react-native.png';
-  imgAngular = 'https://angular.io/assets/images/logos/angular/angular.png';
+  inputValueBlur = '';
 
   constructor() {
-    setTimeout(() => {
-      console.log('timeout is over');
-      this.img = this.imgAngular;
-    }, 5000);
+  }
+
+  onInputChange(event: KeyboardEvent) {
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
+
+  onBlurChange(str: string) {
+    this.inputValueBlur = str;
+  }
+
+  onClickHandle() {
+    console.log('click');
   }
 }
