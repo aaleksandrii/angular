058e927a84dcd7b881039b8f4fb4c04b4528bffe
aleksandrew <andrew.alexandrow97@gmail.com>
2021-02-22 10:53:36 +0200

    binding part two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dynamic title';
-  number = 42 / 2 * 115;
-  arr = [1, 2, 3];
-  obj = { a: 1, b: {c: 2}};
 
+  titleNgModel = 'titleNgModel';
 
-  img = 'https://www.inovex.de/blog/wp-content/uploads/2022/01/one-year-of-react-native.png';
-  imgAngular = 'https://angular.io/assets/images/logos/angular/angular.png';
-
-  constructor() {
-    setTimeout(() => {
-      console.log('timeout is over');
-      this.img = this.imgAngular;
-    }, 5000);
+  onInputHandle = (event: any) => {
+    this.title = event.target.value;
   }
 }
