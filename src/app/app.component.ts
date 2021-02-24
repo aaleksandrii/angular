4c4bbe3e06de27ade17f16bf226a6fcfc72ca8fd
aleksandrew <andrew.alexandrow97@gmail.com>
2021-02-24 11:26:09 +0200

    attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  backgroundToggle = false;
 
+  animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+  posts = [
+    {
+      title: 'lorem',
+      author: 'Andrew',
+      comments: [
+        {name: 'Andros', text: 'Lorem ipsum dolor sit amet.'},
+        {name: 'Andy', text: 'Ipsum dolor sit amet.'},
+        {name: 'Andre', text: 'Dolor sit amet.'},
+      ]
+    },
+    {
+      title: 'Ipsum',
+      author: 'Andrii',
+      comments: [
+        {name: 'Andrea', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, pariatur.'},
+        {name: 'Andrey', text: 'Officia, pariatur.'},
+      ]
+    },
+  ];
 }
