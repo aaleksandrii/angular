2081ae0d0bf1ef32b53c918f7dbe79ecd1ee8f2f
Andrii Aleksandrov <67148738+aleksandrii1@users.noreply.github.com>
2021-03-06 14:54:45 +0200

    add dynamic form (#4)
    Co-authored-by: aleksandrew <andrew.alexandrow97@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+
+import {FormControl, FormGroup } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null),
+    });
+  }
+
+  submit() {
+    console.log('Form Submited', this.form)
+    const formData = this.form.value
+
+    console.log('Form Data: ', formData)
+  }
 }
