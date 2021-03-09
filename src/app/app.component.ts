66c1bdf443cafe48a5fef8849096ea0c690f1c48
Andrii Aleksandrov <67148738+aleksandrii1@users.noreply.github.com>
2021-03-09 11:02:19 +0200

    Lesson/form part 2 (#5)
    * add dynamic form
    
    * add validation|async validation|clear|custom validators for form
    
    Co-authored-by: aleksandrew <andrew.alexandrow97@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import {Component, OnInit} from '@angular/core'
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms'
 
-import {FormControl, FormGroup } from "@angular/forms";
-import { Component, OnInit } from '@angular/core';
+import {MyValidators} from "./my.validators";
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  form: FormGroup;
+  form: FormGroup
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(null),
-    });
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        MyValidators.restrictedEmails
+      ], [
+        MyValidators.uniqEmails
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      address: new FormGroup({
+        country: new FormControl('by'),
+        city: new FormControl('Минск', Validators.required)
+      }),
+      skills: new FormArray([])
+    })
   }
 
   submit() {
-    console.log('Form Submited', this.form)
-    const formData = this.form.value
+    if (this.form.valid) {
+      console.log('Form: ', this.form)
+      const formData = {...this.form.value}
 
-    console.log('Form Data: ', formData)
+      console.log('Form Data:', formData)
+    }
+
+    this.form.reset();
+  }
+
+  setCapital() {
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    }
+
+    const cityKey = this.form.get('address').get('country').value
+    const city = cityMap[cityKey]
+
+    this.form.patchValue({address: {city}})
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    // (<FormArray>this.form.get('skills'))
+    (this.form.get('skills') as FormArray).push(control)
+  }
+
+  getSkills() {
+    return (this.form.get('skills') as FormArray).controls;
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-
-import { AppComponent } from './app.component'
+import {AppComponent} from './app.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -14,9 +13,7 @@ import { AppComponent } from './app.component'
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
