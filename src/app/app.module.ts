394feb47c58d7e3fcf76f3593f607a6380c614cc
aleksandrew <andrew.alexandrow97@gmail.com>
2021-03-10 11:19:23 +0200

    create template for http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
