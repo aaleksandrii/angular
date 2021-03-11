6271a0f04c99c0c5b3c7b6449a715031d8ed86a8
Andrii Aleksandrov <67148738+aleksandrii1@users.noreply.github.com>
2021-03-11 10:56:06 +0200

    improve method post (#7)
    Co-authored-by: aleksandrew <andrew.alexandrow97@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { delay } from "rxjs/operators";
 
 export interface Todo {
   completed: boolean;
@@ -16,12 +17,42 @@ export interface Todo {
 export class AppComponent implements OnInit{
   todos: Todo[] = [];
 
+  todoTitle: string = '';
+
+  loading = false;
+
   constructor(public http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(todos => {
-      console.log(todos)
-      this.todos = todos;
+    this.fetchTodos()
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    this.loading = true;
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false,
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe(response => {
+      console.log('response', response)
+      this.todos.push(response);
+      this.todoTitle = '';
+      this.loading = false;
     });
+  }
+
+  fetchTodos() {
+    this.loading = true;
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(1500))
+      .subscribe(todos => {
+        this.todos = todos;
+        this.loading = false;
+      });
   }
 }
